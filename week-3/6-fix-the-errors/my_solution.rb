d6ca9e5c9f5625537ce21befe6723d6694aaa807
Maria Piper <maria@devbootcamp.com>
2014-09-24 16:25:50 -0400

    added fix the errors challenge

---
+++ week-3/6-fix-the-errors/my_solution.rb
@@ -0,0 +1,169 @@
+# --- error -------------------------------------------------------
+
+"Screw you guys" + "I'm going home" = cartmans_phrase
+
+# (1) what is the name of the file with the error?
+#
+# (2) what is the line number where the error occurs?
+#
+# (3) what is the error message?
+#
+# (4) what is ruby telling you is causing the error?
+#
+# (5) what is the 'type' of the error?
+#
+
+# --- error -------------------------------------------------------
+
+def cartman_hates_rainbows
+  while true
+    puts "What's there to hate about rainbows?"
+end
+
+
+# (1) what is the name of the file with the error?
+#
+# (2) what is the line number where the error occurs?
+#
+# (3) what is the error message?
+#
+# (4) what is ruby telling you is causing the error?
+#
+# (5) what is the 'type' of the error?
+#
+
+# --- error -------------------------------------------------------
+
+south_park
+
+# (1) what is the name of the file with the error?
+#
+# (2) what is the line number where the error occurs?
+#
+# (3) what is the error message?
+#
+# (4) what is ruby telling you is causing the error?
+#
+# (5) what is the 'type' of the error?
+#
+
+# --- error -------------------------------------------------------
+
+cartman()
+
+# (1) what is the name of the file with the error?
+#
+# (2) what is the line number where the error occurs?
+#
+# (3) what is the error message?
+#
+# (4) what is ruby telling you is causing the error?
+#
+# (5) what is the 'type' of the error?
+#
+
+# --- error -------------------------------------------------------
+
+def cartmans_phrase
+  puts "I'm not fat; I'm big-boned!"
+end
+
+cartmans_phrase('I hate Kyle')
+
+# (1) what is the name of the file with the error?
+#
+# (2) what is the line number where the error occurs?
+#
+# (3) what is the error message?
+#
+# (4) what is ruby telling you is causing the error?
+#
+# (5) what is the 'type' of the error?
+#
+
+# --- error -------------------------------------------------------
+
+def cartman_says(offensive_message)
+  puts offensive_message
+end
+
+cartman_says
+
+# (1) what is the name of the file with the error?
+#
+# (2) what is the line number where the error occurs?
+#
+# (3) what is the error message?
+#
+# (4) what is ruby telling you is causing the error?
+#
+# (5) what is the 'type' of the error?
+#
+
+
+
+
+# --- error -------------------------------------------------------
+
+def cartmans_lie(lie, name)
+  puts "#{lie}, #{name}!"
+end
+
+cartmans_lie('A meteor the size of the earth is about to hit Wyoming!')
+
+# (1) what is the name of the file with the error?
+#
+# (2) what is the line number where the error occurs?
+#
+# (3) what is the error message?
+#
+# (4) what is ruby telling you is causing the error?
+#
+# (5) what is the 'type' of the error?
+#
+
+
+# --- error -------------------------------------------------------
+
+5 * "Respect my authoritay!"
+
+# (1) what is the name of the file with the error?
+#
+# (2) what is the line number where the error occurs?
+#
+# (3) what is the error message?
+#
+# (4) what is ruby telling you is causing the error?
+#
+# (5) what is the 'type' of the error?
+#
+
+# --- error -------------------------------------------------------
+
+amount_of_kfc_left = 20/0
+
+# (1) what is the name of the file with the error?
+#
+# (2) what is the line number where the error occurs?
+#
+# (3) what is the error message?
+#
+# (4) what is ruby telling you is causing the error?
+#
+# (5) what is the 'type' of the error?
+#
+
+# --- error -------------------------------------------------------
+
+require_relative "cartmans_essay.md"
+
+# (1) what is the name of the file with the error?
+#
+# (2) what is the line number where the error occurs?
+#
+# (3) what is the error message?
+#
+# (4) what is ruby telling you is causing the error?
+#
+# (5) what is the 'type' of the error?
+#
