a51e32c7651f64905f0e58830657de67e0ccb73f
edisonocean <edisonoceanreef@gmail.com>
2015-01-05 01:53:46 -0800

    committed method for u1.w3 formatted address excercise

--- week-3/exercises/5-format-address/my_solution.rb
+++ week-3/exercises/5-format-address/my_solution.rb
@@ -1,6 +1,10 @@
 # U1.W3: Format an Address
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 
 # Your Solution Below
+
+def make_address (street, city, state, zip)
+return 'You live at ' + street + ', in the beautiful city of ' + city + state + '.' + ' Your zip is ' + zip + '.'
+end
