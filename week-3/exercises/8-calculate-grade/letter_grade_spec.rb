bb7e4f51e771ef0ef56e463eab9526f3432c7434
edisonocean <edisonoceanreef@gmail.com>
2015-01-07 13:58:55 -0800

    committed U1W3 Grade challenge

--- week-3/exercises/8-calculate-grade/letter_grade_spec.rb
+++ week-3/exercises/8-calculate-grade/letter_grade_spec.rb
@@ -1,3 +1,22 @@
+def get_grade(average)
+if
+  average >= 90
+  return "A"
+elsif
+   average >= 80
+  return "B"
+elsif
+   average >= 70
+  return "C"
+elsif
+  average >= 60
+  return "D"
+else
+  return "F"
+
+end
+end
+
 require_relative "my_solution"
 
 describe 'get_grade' do
--- week-3/exercises/8-calculate-grade/my_solution.rb
+++ week-3/exercises/8-calculate-grade/my_solution.rb
@@ -1,6 +1,29 @@
 # U1.W3: Calculate a Grade
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 
 # Your Solution Below
+# Enter
+
+def get_grade(average)
+if
+  average >= 90
+  return "A"
+elsif
+   average >= 80
+  return "B"
+elsif
+   average >= 70
+  return "C"
+elsif
+  average >= 60
+  return "D"
+else
+  return "F"
+
+end
+end
+
+
+
