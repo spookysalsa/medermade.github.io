573c5da53880300e75cf6eb40e7033857aff1546
edisonocean <edisonoceanreef@gmail.com>
2015-01-07 17:10:12 -0800

    committing simple sub-string challenge

--- week-3/exercises/11-simple-substrings/my_solution.rb
+++ week-3/exercises/11-simple-substrings/my_solution.rb
@@ -1,6 +1,13 @@
 # U1.W3: Simple Substrings
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge myself.
+def welcome(address)
+if address.include? "CA"
+  return "Welcome to California"
+else
+  return "You should move to California"
 
+end
+end
 
 # Your Solution Below
