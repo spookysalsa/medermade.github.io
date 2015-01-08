803bfd1f515941b4019bfdf03da51f7571837245
edisonocean <edisonoceanreef@gmail.com>
2015-01-07 19:34:57 -0800

    committed solutions for Concetrating arrays

--- week-3/exercises/16-concatenate-arrays/my_solution.rb
+++ week-3/exercises/16-concatenate-arrays/my_solution.rb
@@ -1,10 +1,10 @@
 # U1.W3: Concatenate Two Arrays
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself ].
 
 
 # Your Solution Below
 
 def array_concat(array_1, array_2)
-  # Your code here
+return array_1 + array_2
 end
