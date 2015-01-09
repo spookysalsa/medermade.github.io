3910bc182d7fac30ebedd816493f644ca8dbd394
edisonocean <edisonoceanreef@gmail.com>
2015-01-08 19:58:08 -0800

    committed solution for pretty triangle

--- week-3/exercises/19-pretty-triangle/my_solution.rb
+++ week-3/exercises/19-pretty-triangle/my_solution.rb
@@ -1,6 +1,8 @@
+
+
 # U1.W3: Pretty Triangle
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [with: Tim Powers].
 
 # print_triangle(rows) prints out a right triangle of +rows+ rows consisting
 # of * characters
@@ -16,5 +18,28 @@
 # Your Solution Below
 
 def print_triangle(rows)
-  # Your code goes here!
+  # above i'm stating that I'm defining a method will have rows number of rows.
+  rows.times do |x|
+    #above i'm stating that rows will now be represented by the variable x.
+
+
+    puts ('*' * x) + '*'
+    #above: this is what I don't understand. I used (x * '*') first
+    #and it didn't work but when I reversed it, it did. I left the
+    #question on our community for clarification.
+    x = x + 1
+    #above: stating that x will have continue to be incrimented by one until it reaches the value of rows.
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
