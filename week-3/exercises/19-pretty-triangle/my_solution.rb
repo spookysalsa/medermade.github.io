2f3d9526893e010fdfad04de53792d97846bc1fb
edisonocean <edisonoceanreef@gmail.com>
2015-01-09 13:04:29 -0800

    committed changes to ch19

--- week-3/exercises/19-pretty-triangle/my_solution.rb
+++ week-3/exercises/19-pretty-triangle/my_solution.rb
@@ -27,8 +27,7 @@ def print_triangle(rows)
     #above: this is what I don't understand. I used (x * '*') first
     #and it didn't work but when I reversed it, it did. I left the
     #question on our community for clarification.
-    x = x + 1
-    #above: stating that x will have continue to be incrimented by one until it reaches the value of rows.
+
   end
 end
 
